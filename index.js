be215e6f4cd87d0cddbce9c5712a1e96a7e01016
Vibek-D <vikiisbck@gmail.com>
2021-09-09 18:28:28 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const cors = require('cors');
+var shortid = require('shortid');
+const Razorpay = require('razorpay');
+
+const razorpay = new Razorpay({
+    "key_id": process.env.KEY_ID,
+    "key_secret": process.env.KEY_SECRET,
+})
+
+const port = process.env.PORT || 3001;
+
+app.set('view engine', 'ejs');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
+app.post('/order', (req, res) => {
+    let options = {
+        "amount": (req.body.amount).toString(),
+        "currency": req.body.currency,
+        "receipt": req.body.id,
+    }
+
+    razorpay.orders.create(options, (err, order) => {
+        res.json(order);
+    })
+})
+
+app.post('/order_complete', (req, res) => {
+    razorpay.payments.fetch(req.body.razorpay_payment_id).then((resp) => console.log(`resp`, resp));
+    if (true) {
+        res.render('success');
+    }
+})
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`)
+})
