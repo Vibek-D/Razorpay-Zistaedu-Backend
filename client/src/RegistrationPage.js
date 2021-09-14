e769f7714abd037157c23036660bfa6ad6dfadd3
Vibek-D <vikiisbck@gmail.com>
2021-09-14 17:37:42 +0530

    transition cjanhed

--- client/src/RegistrationPage.js
+++ client/src/RegistrationPage.js
@@ -81,136 +81,142 @@ function RegistrationPage({ history }) {
 
     return (
         <Box sx={{ fontFamily: 'Exo', fontWeight: '700' }} display="flex" justifyContent="center" alignItems="center" flex="1" flexDirection="column">
-            <Paper elevation={1} sx={{ width: '100vw', height: '150px', mb: '30px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', backgroundColor: 'whitesmoke' }}>
-                <Box display='flex' width="330px" justifyContent="center" backgroundColor="black" borderRadius="20px">
-                    <img style={{marginLeft: '15px'}} src={logo} alt="Logo" />
-                </Box>
-                <Typography variant="h3" color="initial" sx={{ fontFamily: 'Exo', fontWeight: '800', mt: '10px' }}>
-                    REGISTER FOR ZISTA EVENTS
-                </Typography>
-            </Paper>
-            <Paper elevation={1} sx={{ width: '700px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', backgroundColor: 'whitesmoke' }}>
-                <Typography mb='30px' mt="40px" variant="h3" color="grey" sx={{ fontFamily: 'Exo', fontWeight: '800' }}>Registration Form</Typography>
-                {Object.keys(formErrors).length === 0 && isSubmitting && (
-                    <span style={{ fontFamily: 'Exo' }} className="success-msg">Form submitted successfully</span>
-                )}
-                <form onSubmit={handleSubmit} noValidate style={{ width: '500px', position: 'relative' }}>
-                    <div className="form-row">
-                        <label htmlFor="instName">Institution Name</label>
-                        <input
-                            type="instName"
-                            name="instName"
-                            id="instName"
-                            value={formValues.instName}
-                            onChange={handleChange}
-                            className={formErrors.instName && "input-error"}
-                        />
-                        {formErrors.instName && (
-                            <span className="error">{formErrors.instName}</span>
-                        )}
-                    </div>
+            <Box>
+                <Paper elevation={0} sx={{ width: '100vw', height: '100px', mb: '30px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', backgroundColor: 'whitesmoke' }}>
+                    <Box display="flex">
+                        <Box display='flex' width="330px" justifyContent="center" backgroundColor="black" borderRadius="20px">
+                            <img style={{ marginLeft: '15px' }} src={logo} alt="Logo" />
+                        </Box>
+                        <Typography variant="h3" color="initial" sx={{ fontFamily: 'Exo', fontWeight: '800', mt: '10px', ml: '20px' }}>
+                            REGISTER FOR ZISTA EVENTS
+                        </Typography>
+                    </Box>
+                </Paper>
+            </Box>
+            <Box display="flex" justifyContent="center" width="100%" height="100%">
+                <Paper elevation={4} sx={{ width: '700px', height: '800px', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', backgroundColor: 'whitesmoke' }}>
+                    <Typography mb='30px' mt="40px" variant="h3" color="grey" sx={{ fontFamily: 'Exo', fontWeight: '800' }}>Registration Form</Typography>
+                    {Object.keys(formErrors).length === 0 && isSubmitting && (
+                        <span style={{ fontFamily: 'Exo' }} className="success-msg">Form submitted successfully</span>
+                    )}
+                    <form onSubmit={handleSubmit} noValidate style={{ width: '500px', position: 'relative' }}>
+                        <div className="form-row">
+                            <label htmlFor="instName">Institution Name</label>
+                            <input
+                                type="instName"
+                                name="instName"
+                                id="instName"
+                                value={formValues.instName}
+                                onChange={handleChange}
+                                className={formErrors.instName && "input-error"}
+                            />
+                            {formErrors.instName && (
+                                <span className="error">{formErrors.instName}</span>
+                            )}
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="instAddress">Institution Address</label>
-                        <input
-                            type="instAddress"
-                            name="instAddress"
-                            id="instAddress"
-                            value={formValues.instAddress}
-                            onChange={handleChange}
-                            className={formErrors.instAddress && "input-error"}
-                        />
-                        {formErrors.instAddress && (
-                            <span className="error">{formErrors.instAddress}</span>
-                        )}
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="instAddress">Institution Address</label>
+                            <input
+                                type="instAddress"
+                                name="instAddress"
+                                id="instAddress"
+                                value={formValues.instAddress}
+                                onChange={handleChange}
+                                className={formErrors.instAddress && "input-error"}
+                            />
+                            {formErrors.instAddress && (
+                                <span className="error">{formErrors.instAddress}</span>
+                            )}
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="fName">First Name</label>
-                        <input
-                            type="fName"
-                            name="fName"
-                            id="fName"
-                            value={formValues.fName}
-                            onChange={handleChange}
-                            className={formErrors.fName && "input-error"}
-                        />
-                        {formErrors.fName && (
-                            <span className="error">{formErrors.fName}</span>
-                        )}
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="fName">First Name</label>
+                            <input
+                                type="fName"
+                                name="fName"
+                                id="fName"
+                                value={formValues.fName}
+                                onChange={handleChange}
+                                className={formErrors.fName && "input-error"}
+                            />
+                            {formErrors.fName && (
+                                <span className="error">{formErrors.fName}</span>
+                            )}
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="lName">Last Name</label>
-                        <input
-                            type="lName"
-                            name="lName"
-                            id="lName"
-                            value={formValues.lName}
-                            onChange={handleChange}
-                            className={formErrors.lName && "input-error"}
-                        />
-                        {formErrors.lName && (
-                            <span className="error">{formErrors.lName}</span>
-                        )}
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="lName">Last Name</label>
+                            <input
+                                type="lName"
+                                name="lName"
+                                id="lName"
+                                value={formValues.lName}
+                                onChange={handleChange}
+                                className={formErrors.lName && "input-error"}
+                            />
+                            {formErrors.lName && (
+                                <span className="error">{formErrors.lName}</span>
+                            )}
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="email">Email ID</label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="email"
-                            value={formValues.email}
-                            onChange={handleChange}
-                            className={formErrors.email && "input-error"}
-                        />
-                        {formErrors.email && (
-                            <span className="error">{formErrors.email}</span>
-                        )}
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="email">Email ID</label>
+                            <input
+                                type="email"
+                                name="email"
+                                id="email"
+                                value={formValues.email}
+                                onChange={handleChange}
+                                className={formErrors.email && "input-error"}
+                            />
+                            {formErrors.email && (
+                                <span className="error">{formErrors.email}</span>
+                            )}
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="phNumber">Phone Number</label>
-                        <input
-                            type="phNumber"
-                            name="phNumber"
-                            id="phNumber"
-                            value={formValues.phNumber}
-                            onChange={handleChange}
-                            className={formErrors.phNumber && "input-error"}
-                        />
-                        {formErrors.phNumber && (
-                            <span className="error">{formErrors.phNumber}</span>
-                        )}
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="phNumber">Phone Number</label>
+                            <input
+                                type="phNumber"
+                                name="phNumber"
+                                id="phNumber"
+                                value={formValues.phNumber}
+                                onChange={handleChange}
+                                className={formErrors.phNumber && "input-error"}
+                            />
+                            {formErrors.phNumber && (
+                                <span className="error">{formErrors.phNumber}</span>
+                            )}
+                        </div>
 
-                    <div className="form-row">
-                        <label htmlFor="officePhone">Office Phone</label>
-                        <input
-                            type="officePhone"
-                            name="officePhone"
-                            id="officePhone"
-                            value={formValues.officePhone}
-                            onChange={handleChange}
-                            className={formErrors.officePhone && "input-error"}
-                        />
-                        {formErrors.officePhone && (
-                            <span className="error">{formErrors.officePhone}</span>
-                        )}
-                    </div>
+                        <div className="form-row">
+                            <label htmlFor="officePhone">Office Phone</label>
+                            <input
+                                type="officePhone"
+                                name="officePhone"
+                                id="officePhone"
+                                value={formValues.officePhone}
+                                onChange={handleChange}
+                                className={formErrors.officePhone && "input-error"}
+                            />
+                            {formErrors.officePhone && (
+                                <span className="error">{formErrors.officePhone}</span>
+                            )}
+                        </div>
 
-                    <Button sx={{
-                        mb: '70px',
-                        mt: '30px',
-                        bgcolor: '#1d3557',
-                        '&:hover': {
-                            backgroundColor: '#465d7c',
-                            boxShadow: 'none',
-                        },
-                    }} type="submit" variant="contained">Submit</Button>
-                </form>
-            </Paper>
+                        <Button sx={{
+                            mb: '70px',
+                            mt: '30px',
+                            bgcolor: '#EF6C00',
+                            '&:hover': {
+                                backgroundColor: '#FB8C00',
+                                boxShadow: 'none',
+                            },
+                        }} type="submit" variant="contained">Submit</Button>
+                    </form>
+                </Paper>
+            </Box>
         </Box>
     );
 }
