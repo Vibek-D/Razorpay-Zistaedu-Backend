792446dccdb294ef07cc5100c2a13536691ddc9d
Vibek-D <vikiisbck@gmail.com>
2021-11-15 13:43:18 +0530

    done

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ const port = process.env.PORT || 3001;
 
 app.set('view engine', 'ejs');
 app.use(cors());
+app.use(function(request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
